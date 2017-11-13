aa5add2bd4f06db63017fca605a764257ac4fc87
Jelle van Leeuwen <info@jellevl.nl>
2017-11-13 14:32:07 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Facades/Cspreporter.php
+++ src/Facades/Cspreporter.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
 
 class Cspreporter extends Facade
 {
-	protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor()
     {
         return 'cspreporter.cspreporter';
     }
