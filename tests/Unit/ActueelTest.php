cb155c9c49075d14ad1f5b3bcd3fe0512f4280ed
jvleeuwen <info@jellevl.nl>
2017-11-14 13:37:11 +0100

    changed the test to the new facade

--- tests/Unit/ActueelTest.php
+++ tests/Unit/ActueelTest.php
@@ -2,8 +2,9 @@
 
 namespace Jvleeuwen\Cspreporter\Tests\Unit;
 
-use Orchestra\Testbench\TestCase;
-use Jvleeuwen\Cspreporter\Cspreporter;
+use PHPUnit\Framework\TestCase;
+// use Orchestra\Testbench\TestCase;
+use Jvleeuwen\Cspreporter\CspreporterService as cspreporter;
 
 class ActueelTest extends TestCase
 {
@@ -15,39 +16,47 @@ class ActueelTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->feed = new Cspreporter();
+        $this->cspreporter = new cspreporter();
         $this->file = 'http://cspreporter.nl/rss/actueel/';
     }
 
     /** @test */
     public function it_can_load_rss()
     {
-        $this->assertInternalType('object', $this->feed->file($this->file));
+        // $this->cspreporter = new cspreporter();
+        $this->assertInternalType('array', $this->cspreporter->uri($this->file));
     }
 
     /** @test */
     public function it_can_get_actual_test_xml_from_tests_dir()
     {
-        $this->assertCount(1, $this->feed->file($this->file));
+        $this->assertCount(1, $this->cspreporter->file($this->file));
     }
 
     /** @test */
     public function it_can_parse_rss()
     {
-        $this->assertInternalType('array', $this->feed->ParseRss($this->feed->file($this->file)));
+        $this->assertInternalType('array', $this->cspreporter->ParseRss($this->cspreporter->file($this->file)));
     }
 
     /** @test */
     public function it_can_run_the_full_cycle()
     {
-        $this->assertInternalType('array', $this->feed->uri($this->file));
+        $this->assertInternalType('array', $this->cspreporter->uri($this->file));
     }
 
     /** @test */
     public function it_can_not_parse_uri_feed()
     {
-        $this->assertSame('invalid XML', $this->feed->uri('i dont exist'));
+        $this->assertSame('invalid XML', $this->cspreporter->uri('i dont exist'));
     }
+
+
+
+
+
+
+
 
     // public function testUpdateOrInsertRssItemInToDatabase()
     // {
