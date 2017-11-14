bf72dd0fd71b51e83143b27c494d409febe4730a
Jelle van Leeuwen <info@jellevl.nl>
2017-11-14 10:20:13 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Facade.php
+++ src/Facade.php
@@ -2,10 +2,10 @@
 
 namespace Jvleeuwen\Cspreporter;
 
-class Cspreporter extends \Illuminate\Support\Facades\Facade
+class Facade extends \Illuminate\Support\Facades\Facade
 {
     protected static function getFacadeAccessor()
     {
-        return Cspreporter::class;
+        return self::class;
     }
 }
