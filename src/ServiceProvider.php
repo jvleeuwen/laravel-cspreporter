48559a82e1abb04f188c14686f153392fd8730cf
jvleeuwen <info@jellevl.nl>
2017-11-14 12:10:41 +0100

    pffF

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,8 +8,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->app->singleton('cspreporter', function () {
             return new Cspreporter($this->app);
-        }
-        );
+        });
 
         $this->app->alias('cspreporter', Cspreporter::class);
     }
