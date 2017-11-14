e2809c6300a649cd6dfb9bac246d5d5d8556fd5f
jvleeuwen <info@jellevl.nl>
2017-11-14 11:37:11 +0100

    second try

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,17 +6,9 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
-<<<<<<< HEAD
         $this->app->singleton('cspreporter', function () {
                 return new Cspreporter($this->app);
             }
-=======
-        $this->app->singleton(Cspreporter::class, function () {
-            $cspreporter = new Cspreporter($this->app);
-
-            return $cspreporter;
-        }
->>>>>>> bf72dd0fd71b51e83143b27c494d409febe4730a
         );
 
         $this->app->alias('cspreporter', Cspreporter::class);
