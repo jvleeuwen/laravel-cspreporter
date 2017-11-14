2baf69b1bf0fb7d4c4282af44d4b7d482896f7b9
jvleeuwen <info@jellevl.nl>
2017-11-14 10:03:48 +0100

    registering service provider

--- src/CspreporterServiceProvider.php
+++ src/CspreporterServiceProvider.php
@@ -14,13 +14,13 @@ class CspreporterServiceProvider extends ServiceProvider
 
     public function register()
     {
-        // $this->app->singleton('cspreporter.cspreporter', function (Container $app) {
-        //     return new Cspreporter();
-        // });
+        $this->app->singleton('cspreporter.cspreporter', function (Container $app) {
+            return new Cspreporter();
+        });
 
-        // $this->app->alias('cspreporter.cspreporter', Cspreporter::class);
+        $this->app->alias('cspreporter.cspreporter', Cspreporter::class);
 
-        $this->app->register('Jvleeuwen\Cspreporter');
-        $this->app->alias('Cspreporter', 'Jvleeuwen\Cspreporter/Facades');
+        // $this->app->register('Jvleeuwen\Cspreporter');
+        // $this->app->alias('Cspreporter', 'Jvleeuwen\Cspreporter/Facades');
     }
 }
